8c70ba5092c9f47fe10d63e0775010ee5aada94f
malavrana1 <ranam211197@gmail.com>
2025-11-06 04:16:05 -0500

    Updates

---
+++ precache-manifest.ddff18bc5af626673dfa76f5e44763e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8deb07e5a9b5597f4fb9fd67302450",
+    "url": "/MR_Orbit_Studio/index.html"
+  },
+  {
+    "revision": "60e3a42830da5c0c25f9",
+    "url": "/MR_Orbit_Studio/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "8583e5273334b8f14edd",
+    "url": "/MR_Orbit_Studio/static/css/main.cffef81c.chunk.css"
+  },
+  {
+    "revision": "60e3a42830da5c0c25f9",
+    "url": "/MR_Orbit_Studio/static/js/2.0c209d1b.chunk.js"
+  },
+  {
+    "revision": "8583e5273334b8f14edd",
+    "url": "/MR_Orbit_Studio/static/js/main.a1659a99.chunk.js"
+  },
+  {
+    "revision": "9d00285d813a6aafa0bc",
+    "url": "/MR_Orbit_Studio/static/js/runtime~main.a61076d5.js"
+  },
+  {
+    "revision": "a65548b32f6102d28c4745ee3f78ed63",
+    "url": "/MR_Orbit_Studio/static/media/Malav_Rana_Frontend_Developer.a65548b3.pdf"
+  },
+  {
+    "revision": "3ccb49bddb41d51b9395c5c0fc6674c4",
+    "url": "/MR_Orbit_Studio/static/media/bg2.3ccb49bd.png"
+  }
+]);
